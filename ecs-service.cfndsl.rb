e99f3fde93a712aeb7c954eb2ed71dc67ae9d81d
Lohgan Nash <lohgannash@gmail.com>
2019-11-19 15:01:56 +1100

    Export service name (#40)
    * Fix for having multiple links
    
    * Fix array insertion
    
    * Insert was invalid, push instead:
    
    * Add test
    
    * Various refactoring & fixes around passing in arrays to taskdef parameters
    
    * Add export for servicename
    
    * Move output to end of file

--- ecs-service.cfndsl.rb
+++ ecs-service.cfndsl.rb
@@ -1,5 +1,7 @@
 CloudFormation do
 
+  export = defined?(export_name) ? export_name : component_name
+
   awsvpc_enabled = false
   if defined?(network_mode) && network_mode == 'awsvpc'
     awsvpc_enabled = true
@@ -554,5 +556,10 @@ CloudFormation do
     }
 
   end
+
+  Output("ServiceName") {
+    Value(FnGetAtt(:Service, :Name))
+    Export FnSub("${EnvironmentName}-#{export}-ServiceName")
+  }
 
 end
